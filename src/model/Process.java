7c916309c788f78b6970456ee9734c2f282b577c
ahmedmedhat444 <ahmedmedhatkamal2005@gmail.com>
2026-05-05 00:38:42 +0300

    process class edited

--- src/model/Process.java
+++ src/model/Process.java
@@ -1,31 +1,28 @@
-package src.model;
 public class Process {
     public int pid;
     public int arrivalTime;
     public int burstTime;
     public int priority;
-
     public int remainingTime;
-
     public int completionTime;
     public int waitingTime;
     public int turnaroundTime;
     public int responseTime;
-
     public boolean started;
     public int firstRunTime;
+
+    public Process() {}
 
     public Process(int pid, int arrivalTime, int burstTime, int priority) {
         this.pid = pid;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.priority = priority;
-
         this.remainingTime = burstTime;
-
         this.started = false;
         this.firstRunTime = -1;
     }
+
     public void reset() {
         this.remainingTime = burstTime;
         this.started = false;
@@ -35,5 +32,21 @@ public class Process {
         this.turnaroundTime = 0;
         this.responseTime = 0;
     }
-}
 
+
+
+    public int getPid() { return pid; }
+    public int getArrivalTime() { return arrivalTime; }
+    public int getBurstTime() { return burstTime; }
+    public int getPriority() { return priority; }
+    public int getCompletionTime() { return completionTime; }
+    public int getWaitingTime() { return waitingTime; }
+    public int getTurnaroundTime() { return turnaroundTime; }
+    public int getResponseTime() { return responseTime; }
+
+    public void setWaitingTime(int waitingTime) { this.waitingTime = waitingTime; }
+    public void setTurnaroundTime(int turnaroundTime) { this.turnaroundTime = turnaroundTime; }
+    public void setResponseTime(int responseTime) { this.responseTime = responseTime; }
+
+    public void setPid(int pid) { this.pid = pid; }
+}
